7031469747165309a53e204655ed429ffd6a96f9
dibyenduonline <dibyendu.das2@outlook.com>
2019-09-17 22:48:31 -0400

    Add files via upload

---
+++ APIRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace DemoApi001
+{
+    /*
+    [Serializable]
+    [DataContract]*/
+    public class APIRequest
+    {
+    //    [DataMember]
+        public string method1 { get; set; }
+      //  [DataMember]
+        public string dataDetails { get; set; }
+    }
+}
---
+++ DemoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApi001
+{
+    [Route("api/[controller]/[action]")]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public string MyRequets()
+        {
+            return " Processing at Server";
+        }
+
+
+        [HttpPost]
+        public string MyRequets1()
+        {
+            return " Processing at Server as post";
+        }
+
+
+        [HttpPost]
+        public string MyRequets2([FromBody]APIRequest input)
+        {
+            return " Processing at Server as post with method as "  + input.method1;
+        }
+
+
+
+    }
+}
